2fe01392e6384d454e53332d9138a95ae74d86f6
Assanali18 <ustaevasanali@gmail.com>
2024-10-21 02:40:28 +0500

    change go mod 10

--- backend/internal/database/connect.go
+++ backend/internal/database/connect.go
@@ -1,24 +1,18 @@
 package database
 
 import (
-	"backend/internal/config"
 	"backend/internal/models"
 	"fmt"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"log"
+	"os"
 )
 
 var DB *gorm.DB
 
 func ConnectDB() error {
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-		config.AppConfig.DBHost,
-		config.AppConfig.DBUser,
-		config.AppConfig.DBPassword,
-		config.AppConfig.DBName,
-		config.AppConfig.DBPort,
-	)
+	dsn := os.Getenv("DATABASE_URL")
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return fmt.Errorf("failed to connect to database: %w", err)
